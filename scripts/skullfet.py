3295c4370d557c141bdcf657f96424048837279d
Uri Shaked <uri@urishaked.com>
2021-12-18 23:34:49 +0200

    feat: NAND gate

--- scripts/skullfet.py
+++ scripts/skullfet.py
@@ -2,10 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import numpy as np
+import sys
 from skullshapes import skull, bones
 
 unit = np.eye(3)
-
+rotate90 = np.array([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
+rotate180 = rotate90.dot(rotate90)
+rotate270 = np.array([[0, -1, 0], [-1, 0, 0], [0, 0, 1]])
 
 def translate(x, y):
     return np.array([[1, 0, x], [0, 1, y], [0, 0, 1]])
@@ -55,16 +58,28 @@ def draw(rects, matrix=unit):
         maxy = round(max(y1, y2))
         print("rect {} {} {} {}".format(minx, miny, maxx, maxy))
 
+draw_4_mosfets = '-4' in sys.argv
+nwell_padding = 1 if draw_4_mosfets else 2
 
 print("magic")
 print("tech sky130A")
 print("timestamp 1638034600")
+
 print("<< ndiff >>")
 draw(skull, scale(27).dot(vflip))
+if draw_4_mosfets:
+    draw(skull, scale(27).dot(translate(-20, -39)).dot(rotate90))
+
 print("<< metal1 >>")
 draw(bones, scale(27).dot(translate(0, -34)))
+
 print("<< nwell >>")
-draw(bounds(skull, pad=2), scale(27).dot(translate(0, -54)))
+draw(bounds(skull, pad=nwell_padding), scale(27).dot(translate(0, -54)))
+if draw_4_mosfets:
+    draw(bounds(skull, pad=nwell_padding), scale(27).dot(translate(45, -14)).dot(rotate270))
+
 print("<< pdiff >>")
 draw(skull, scale(27).dot(translate(0, -54)))
+if draw_4_mosfets:
+    draw(skull, scale(27).dot(translate(45, -14)).dot(rotate270))
 print("<< end >>")
