bfd3adfcd983374cc38ba1d38b9e4a3cf249ae2d
Uri Shaked <uri@urishaked.com>
2022-12-04 12:23:26 +0200

    fix: klayout python script to fix Nplus/COMP spacing

--- skullfet_gf180.py
+++ skullfet_gf180.py
@@ -12,10 +12,28 @@ layout.read(infile)
 layout.dbu = 0.005
 
 # Surround the design with a dualgate box:
-dualgate_layer = 55
+dualgate_layer = layout.layer(55, 0)
 dualgate_pad = 1000
-bbox = layout.top_cell().bbox()
-dualgate_box = pya.Box(bbox.left - dualgate_pad, bbox.bottom - dualgate_pad, bbox.right + dualgate_pad, bbox.top + dualgate_pad)
-layout.top_cell().shapes(layout.layer(dualgate_layer, 0)).insert(dualgate_box)
+dualgate_box = layout.top_cell().bbox().enlarged(pya.Point(dualgate_pad, dualgate_pad))
+layout.top_cell().shapes(dualgate_layer).insert(dualgate_box)
+
+# Add padding around COMP layer where NPLUS is present
+comp_layer = layout.layer(22, 0)
+nplus_layer = layout.layer(32, 0)
+comp_padding = 140
+it = layout.top_cell().begin_shapes_rec(comp_layer)
+new_boxes = []
+remove_boxes = []
+while not it.at_end():
+  for shape in layout.top_cell().shapes(nplus_layer).each_overlapping(it.shape().bbox()):
+    if shape.is_box():
+      remove_boxes.append(shape)
+      nplus_box = shape.bbox().enlarged(pya.Point(comp_padding, comp_padding))
+      new_boxes.append(nplus_box)
+  it.next()
+for box in remove_boxes:
+  layout.top_cell().shapes(nplus_layer).erase(box)
+for box in new_boxes:
+  layout.top_cell().shapes(nplus_layer).insert(box)
 
 layout.write(outfile)
